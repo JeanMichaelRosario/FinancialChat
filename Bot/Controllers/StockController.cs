5ae0475f88c8dc646ff6df7f956008c7ef6d214b
jean.rosario <jean.rosario@intapp.com>
2022-09-20 14:20:31 -0400

    adding decoupled bot

---
+++ Bot/Controllers/StockController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        
+        public StockController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet(Name = "stock={stockCode}")]
+        public async Task Get(string stockCode)
+        {
+            var url = _configuration.GetValue<string>("ChatUrl");
+            await Helper.CsvHelper.PostStockQuoteInformationToAllChatRooms(url, stockCode);
+        }
+    }
+}
---
+++ Bot/Helper/CsvHelper.cs
@@ -0,0 +1,84 @@
+﻿using CsvHelper;
+using System.Globalization;
+using System.Net;
+
+namespace Bot.Helper
+{
+    public static class CsvHelper
+    {
+        public static async Task PostStockQuoteInformationToAllChatRooms(string chatUrl, string stockCode)
+        {
+            var url = $"https://stooq.com/q/l/?s={stockCode}&f=sd2t2ohlcv&h&e=csv";
+            var botMessage = GetBotMessage(url);
+            await PostBotMessage(botMessage, chatUrl);
+        }
+
+        private static Stream GetCsvFile(string url)
+        {
+            try
+            {
+                var req = WebRequest.Create(url);
+                var response = req.GetResponse();
+                return response.GetResponseStream();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Request error", ex);
+            }
+        }
+
+        private static Stock? GetStockData(string url)
+        {
+            var stream = GetCsvFile(url);
+
+            using (var reader = new StreamReader(stream))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var record = csv.GetRecords<Stock>().First();
+                if (record.Open != "N/D")
+                {
+                    return record;
+                }
+                return null;
+            }
+        }
+
+        private static string GetBotMessage(string url)
+        {
+            var stock = GetStockData(url);
+            if (stock is null)
+            {
+                throw new Exception("Quote not found!");
+            }
+            return $"{stock.Symbol} quote is ${stock.Open} per share";
+        }
+
+        private static async Task PostBotMessage(string chatUrl, string message)
+        {
+            var values = new Dictionary<string, string>
+            {
+                { "message", message },
+            };
+            
+            var client = new HttpClient();
+            
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync(chatUrl, content);
+        }
+
+
+    }
+
+    internal class Stock
+    {
+        public string Symbol { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Open { get; set; }
+        public string High { get; set; }
+        public string Low { get; set; }
+        public string Close { get; set; }
+        public string Volume { get; set; }
+    }
+}
